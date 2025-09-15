1b1993621fb37f548b0f8543c6f7bec5e4cf1593
Allen-Reiley <rbadigital093@gmail.com>
2025-09-15 13:06:58 +0200

    my-first-real-commit

---
+++ src/App.jsx
@@ -0,0 +1,126 @@
+import React, { use, useEffect, useState } from 'react';
+import Search from './components/Search.jsx';
+import Loader from './components/Loader.jsx';
+import MovieCard from './components/MovieCard.jsx';
+import {useDebounce} from 'react-use';
+import Loading from './components/Loading.jsx';
+import { updateSearchCount } from './appwrite.js';
+
+const BASE_API_URL = 'https://api.themoviedb.org/3';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+};
+
+const App = () => {
+
+  const [loadings, setLoadings] = useState(false);
+
+  useEffect(() => {
+    setLoadings(true);
+    setTimeout(() => {
+      setLoadings(false);
+    }, 3000);
+  }, []);
+
+  const [searchTerm,setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [movieList,setMovieList] = useState([]);
+  const [isLoading,setIsLoading] = useState(false);
+  const [debouncedSearchTerm,setDebouncedSearchTerm] = useState('');
+
+  useDebounce(() => {
+    setDebouncedSearchTerm(searchTerm);
+  }, 500, [searchTerm]);
+
+  const fetchMovies = async ( query = '') => {
+    setIsLoading(true);
+    setErrorMessage('');
+
+    try{
+      const endpoint = query
+      ? `${BASE_API_URL}/search/movie?query=${encodeURIComponent(query)}`
+       : `${BASE_API_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if(!response.ok){
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      
+      if(data.Response === 'False'){
+        setErrorMessage(data.Error || 'Error fetching movies');
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || []);
+
+      if(query && data.results.length > 0){
+        await updateSearchCount(query, data.results[0]);
+      }  
+      
+
+    } catch(error){
+      console.error('Error fetching movies:', error);
+      setErrorMessage('Failed to fetch movies. Please try again later.');
+      
+    } finally{
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies(searchTerm);
+  },[searchTerm]);
+
+  return (
+    <main>
+      <div className='pattern' />
+      <div className='wrapper'>
+        {
+          loadings ? <Loading /> : null
+        }
+        <header>
+          <img src='browsflix.png' />
+          <img src='./hero.png' alt='Hero Banner' />
+          <h1>
+            Find <span className='text-gradient'>Movies</span> You'll Enjoy Without The Hassle
+          </h1>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          
+        </header>
+
+        <section className='all-movies'>
+          <h2 className='mt-[40px]'>All Movies</h2>
+          
+          {isLoading ? (
+            <Loader />
+          ) : errorMessage ? (
+            <p className='text-red-500'>{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )
+            }
+
+        </section>
+        
+      </div>
+    </main>
+  )
+}
+export default App;
+
